8d078ec37987d252a5892e658d14db2fb92fb03e
Medscootsman <murraylyne2@Gmail.com>
2016-10-17 14:12:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <meta charset="utf-16">
+    <meta name="test site">
+    <title>test</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+
+</head>
+
+<body>
+
+<h1>PHP work</h1>
+<br>
+
+<h3> This is my php work for the labwork we had to do today.</h3>
+
+<br>
+
+
+<p>Where would you like to say hello?</p>
+
+<ol>
+    <li><a href="">Earth</a></li>
+    <li><a href="">Mars</a></li>
+    <li><a href="">Uranus</a></li>
+</ol>
+<?php
+
+?>
+
+</body>
+</html>
