924733f6c9827efe700f10a5211d7ca12250b3a6
Medscootsman <murraylyne2@Gmail.com>
2016-10-17 16:39:23 +0100

    COMICS

--- insertsuperhero.php
+++ insertsuperhero.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1607084
- * Date: 17/10/2016
- * Time: 16:12
- */
+$SERVER = "eu-cdbr-azure-north-e.cloudapp.net";
+$USERNAME = "baa0a9a8041f1c";
+$PASSWORD = "ea0c77dc";
+$DATABASE = "marvel";
+
+$db = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+
+if (!$db) {
+    die("Could not connect:". mysqli_connect_error());
+
+}
+
+$sql = "SELECT * FROM superheros";
+$result = "";
+?>
